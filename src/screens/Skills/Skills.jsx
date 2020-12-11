48d3729770934771d9f667a4b1dd0eef63a2cdfe
Mac May <tmcpbmay@gmail.com>
2020-12-10 21:53:49 -0500

    add content and base styling to skills

--- src/screens/Skills/Skills.jsx
+++ src/screens/Skills/Skills.jsx
@@ -1,13 +1,35 @@
 import React from "react";
+import "./Skills.css";
 
 export default function Skills(props) {
   return (
     <div>
       <h1>My many and varied talents</h1>
-      <h3>Languages</h3>
-      <h3>Libraries</h3>
-      <h3>Frameworks</h3>
-      <h3>Databases</h3>
+      <div id="skillz">
+        <h3>Languages</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Ruby</li>
+          <li>Python</li>
+        </ul>
+        <h3>Libraries</h3>
+        <ul>
+          <li>React</li>
+          <li>Mongoose</li>
+        </ul>
+        <h3>Frameworks</h3>
+        <ul>
+          <li>Express</li>
+          <li>Rails</li>
+        </ul>
+        <h3>Databases</h3>
+        <ul>
+          <li>MongoDB</li>
+          <li>Postgres</li>
+        </ul>
+      </div>
     </div>
   );
 }
