105917c9dbaba48975ac0abdecbb2e15bcd13a8c
Mac May <tmcpbmay@gmail.com>
2020-12-10 23:06:12 -0500

    all base content added to site. Barebones styling done.

--- src/screens/Education/Education.jsx
+++ src/screens/Education/Education.jsx
@@ -2,5 +2,19 @@ import React from "react";
 import "./Education.css";
 
 export default function Education(props) {
-  return <div>This is Education</div>;
+  return (
+    <div>
+      <h1>Book Learnin'</h1>
+      <h3>General Assembly (2020)</h3>
+      <p>Software Engineering Immersive. Completion certificate.</p>
+      <h3>Drexel University (2017-2020)</h3>
+      <p>Post-baccallaurate certificate in Computer Science.</p>
+      <p>
+        Graduate courses include: Dependable Software Systems, Intro to AI,
+        Programming Languages, Computer Networks.
+      </p>
+      <h3>Quinnipiac University (2000-2005)</h3>
+      <p>BA in Mass Communications, minor in International Studies.</p>
+    </div>
+  );
 }
