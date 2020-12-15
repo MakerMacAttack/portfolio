fb8f5b20c37dae3fef140e1c254a9a9fcafe55b5
Mac May <tmcpbmay@gmail.com>
2020-12-15 17:10:43 -0500

    temporary adjustment to contact form during feedback period

--- src/services/ContactForm.js
+++ src/services/ContactForm.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
 const submitContact = async (fields) => {
-  const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/messages`;
+  const airtableURL = `https://api.airtable.com/v0/appMDdUOw44Hx9OCV/messages`;
   await axios.post(
     airtableURL,
     { fields },
     {
       headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        Authorization: `Bearer keyNjK8uUlpY2OP8b`,
       },
     }
   );
 };
 
 export default submitContact
+
+
+// ${ process.env.REACT_APP_AIRTABLE_BASE }
+// ${ process.env.REACT_APP_AIRTABLE_KEY }
