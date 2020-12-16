6b8e9b09d8924950934904c0c6612c76fad0a9b8
Mac May <tmcpbmay@gmail.com>
2020-12-15 23:12:59 -0500

    style the links on the about page.

--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -28,27 +28,42 @@ export default function About(props) {
         </div>
       </div>
       <h3 className="sub-title">My Journey</h3>
-      <p className="body-text">
+      <p className="body-text bio">
         As a youth, I had two passions. Videography, and computer programming. I
         made the tough decision to pursue video first, and{" "}
-        <Link to="/experience">got a job out of college</Link> in live,
-        nationally-broadcast news. I learned and grew a lot in my decade in that
-        industry, and when it came time for a new path in life, I decided I
-        could experience more by exploring my second passion.
+        <Link to="/experience" className="about-link">
+          got a job out of college
+        </Link>{" "}
+        in live, nationally-broadcast news. I learned and grew a lot in my
+        decade in that industry, and when it came time for a new path in life, I
+        decided I could experience more by exploring my second passion.
       </p>
-      <p className="body-text">
+      <p className="body-text bio">
         Now, after studying the basics, graduate courses, and{" "}
-        <Link to="/education">an intense coding academy</Link>, I'm ready to
-        bring my own unique insights and <Link to="/skills">skills</Link> to the
-        field of software engineering, as the world around us is shaped more and
-        more every day by ubiquitous code.
+        <Link to="/education" className="about-link">
+          an intense coding academy
+        </Link>
+        , I'm ready to bring my own unique insights and{" "}
+        <Link to="/skills" className="about-link">
+          skills
+        </Link>{" "}
+        to the field of software engineering, as the world around us is shaped
+        more and more every day by ubiquitous code.
       </p>
-      <p className="body-text">
+      <p className="body-text bio">
         For an example of how I combine my two passions, check out the{" "}
-        <Link to="/compsci">CS section</Link>.
+        <Link to="/compsci" className="about-link">
+          CS section
+        </Link>
+        .
       </p>
-      <p className="body-text">
-        <a href={"../assets/MacMayResume.pdf"} target="_blank" rel="noreferrer">
+      <p className="body-text bio">
+        <a
+          href={"../assets/MacMayResume.pdf"}
+          target="_blank"
+          rel="noreferrer"
+          className="about-link"
+        >
           Click here
         </a>{" "}
         to view or download a copy of my resume.
